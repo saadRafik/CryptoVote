4a10fd8eba354df8117d34cac44b36e2805c605d
caronf <florimond.caron@etu.umontpellier.fr>
2025-03-23 23:03:20 +0100

    final commit

--- frontend/app/constants/index.js
+++ frontend/app/constants/index.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1"
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 export const abi = [
   {
     "inputs": [],
